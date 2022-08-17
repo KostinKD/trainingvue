cc6eedae27c968e1a38110ecc0464fccfebc8b75
KostinKD <omroners@gmail.com>
2022-08-17 17:26:21 +0300

    Creating and trying modal and teleport in VUE

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,25 @@
       <h2>{{ $translatewords('app.title') }}</h2>
       <button class="btn" @click="$alert('Its work!')">Alert</button>
       <button class="btn" @click="changeLang">{{ $translatewords('app.changeBtn') }}</button>
+      <button class="btn" @click="showModal = true">Показать модальное окно</button>
     </div>
   </div>
+  <teleport to="#modal">
+    <app-modal v-if="showModal" @closeModal="showModal = false"></app-modal>
+  </teleport>
 </template>
 
 <script>
 import focuscustom from "@/directives/focuscustom";
 import colorDirective from "@/directives/colorDirective";
+import appModal from "@/components/AppModal";
 export default {
+  components: {appModal},
+  data(){
+    return {
+      showModal: false
+    }
+  },
   inject: ['changetranslatewords'],
   methods: {
     changeLang(){
