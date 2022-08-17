787ce99449a1650a6a800b365f06fade7d09da10
KostinKD <omroners@gmail.com>
2022-08-17 16:20:14 +0300

    Creating change lang

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <div class="card">
-      <h2>Как работают плагины во VUE </h2>
-
+      <h2>{{ $translatewords('app.title') }}</h2>
       <button class="btn" @click="$alert('Its work!')">Alert</button>
+      <button class="btn" @click="changeLang">{{ $translatewords('app.changeBtn') }}</button>
     </div>
   </div>
 </template>
@@ -12,7 +12,13 @@
 import focuscustom from "@/directives/focuscustom";
 import colorDirective from "@/directives/colorDirective";
 export default {
-
+  inject: ['changetranslatewords'],
+  methods: {
+    changeLang(){
+      this.changetranslatewords('eng')
+      this.$forceUpdate()
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,7 +6,21 @@ import translateplugin from "@/translateplugin";
 
 const app = createApp(App)
 
-app.use(translateplugin)
+const ru = {
+    app: {
+        title: 'Как работают плагины во VUE',
+        changeBtn: 'Сменить язык'
+    }
+}
+
+const eng = {
+    app: {
+        title: 'How plugin works in VUE',
+        changeBtn: 'Change lang'
+    }
+}
+
+app.use(translateplugin, {ru,eng})
 app.mount('#app')
 // directive(
 //     //ГЛОБАЛЬНАЯ РЕГИСТАРЦИЯ ДИРЕКТИВЫ
