00245ef218380924491f61a3c124fbc45932da5b
KostinKD <omroners@gmail.com>
2022-08-29 21:38:17 +0300

    07 main page

---
+++ src/components/request/RequestModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <form name="requestmodal">
+    <div class="form-control">
+      <label for="">ФИО</label>
+      <input type="text">
+      <br>
+      <label for="">Телефон</label>
+      <input type="text">
+      <br>
+      <label for="">Сумма</label>
+      <input type="text">
+      <br>
+      <label for="">Статус</label>
+      <select name="status">
+        <option value="active">Активная</option>
+        <option value="waiting">В ожидании</option>
+      </select>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="home">
-  </div>
+  <app-page title="Список заявок">
+    <template #header>
+      <button class="btn primary" @click="modal = true">Создать</button>
+    </template>
+    <request-table :requests="[]"></request-table>
+
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
+        <request-modal>
+
+        </request-modal>
+      </app-modal>
+    </teleport>
+  </app-page>
 </template>
 
 <script>
+
+import AppPage from "@/components/ui/AppPage";
+import RequestTable from "@/components/request/RequestTable";
+import AppModal from "@/components/ui/AppModal";
+import RequestModal from "@/components/request/RequestModal";
+import {ref} from "vue";
 export default {
   name: 'Home',
+  components: {AppPage,RequestTable,AppModal,RequestModal},
+  setup(){
+    const modal = ref(false)
+
+
+
+    return{
+      modal,
+
+    }
+  }
 }
 </script>
