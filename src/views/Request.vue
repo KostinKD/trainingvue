4650b75e99778401ce42829d8b7b2c0d53f1b108
KostinKD <omroners@gmail.com>
2022-08-31 19:54:07 +0300

    14 update and delete

--- src/views/Request.vue
+++ src/views/Request.vue
@@ -5,6 +5,18 @@
     <p><strong>Телефон</strong>: {{request.phone}}</p>
     <p><strong>Сумма</strong>: {{currency(request.summa)}}</p>
     <p><strong>Статус</strong>: <AppStatus :type="request.status"/> </p>
+
+
+    <select name="status" id="status" v-model="status">
+      <option value="active">Активная</option>
+      <option value="pending">В ожидании</option>
+      <option value="done">Завершен</option>
+      <option value="cancelled">Отменен</option>
+    </select>
+
+
+    <button class="btn danger" @click="remove">Удалить</button>
+    <button class="btn primary" @click="update" v-if="hasChanges">Обновить</button>
   </app-page>
   <h3 v-else class="text-center text-white">Заявки с ID = {{ id }} нет</h3>
 </template>
@@ -13,11 +25,12 @@
 import AppPage from "@/components/ui/AppPage";
 
 import {useRoute} from "vue-router";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 import {useStore} from "vuex";
 import AppStatus from "@/components/ui/AppStatus";
 import AppLoader from "@/components/ui/AppLoader";
 import {currency} from "@/utils/currency-formator";
+import router from "@/router";
 
 export default {
   components: {
@@ -28,16 +41,32 @@ export default {
     const store = useStore()
     const loading = ref(true)
     const request = ref({})
+    const status = ref()
 
+
+    const remove = async () => {
+      await store.dispatch('request/remove', route.params.id)
+      router.push('/')
+    }
+    const update = async () => {
+      const data = {...request.value, status: status.value, id: route.params.id}
+      await store.dispatch('request/update', data)
+      request.value.status = status.value
+    }
     onMounted(async ()=>{
       loading.value = true
       request.value = await store.dispatch('request/loadById', route.params.id)
+      status.value = request.value?.status
       loading.value = false
     })
 
+    const hasChanges = computed(()=>
+      request.value.status !== status.value
+    )
+
 
     return {
-      loading,request,AppStatus,AppLoader,AppPage,id: route.params.id,currency
+      loading,request,AppStatus,AppLoader,AppPage,id: route.params.id,currency,remove, update, status, hasChanges
     }
   }
 }
