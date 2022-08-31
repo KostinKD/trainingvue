dae623b45a68fdfd33093eb600bec8aefd072103
KostinKD <omroners@gmail.com>
2022-08-31 20:17:08 +0300

    16 add sidebar

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,8 @@ export default createStore({
   plugins,
   state() {
     return {
-      message: null
+      message: null,
+      sidebar: false
     }
   },
   getters: {
@@ -25,6 +26,12 @@ export default createStore({
     },
     clearMessage(state){
       state.message = null
+    },
+    openSidebar(state){
+      state.sidebar = true
+    },
+    closeSidebar(state){
+      state.sidebar = false
     }
   },
   actions: {
